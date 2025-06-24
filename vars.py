b96a7e81c9d978f557c4668e861371e5b72e5ba7
yassuhacks750 <yassudj8085231272@gmail.com>
2025-06-25 02:13:47 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,6 +7,7 @@ API_ID = int(environ.get("API_ID", "22849789"))
 API_HASH = environ.get("API_HASH", "0fc127c6055acd59f00ec6c229e1e3c4")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7627920473:AAFnx6BHFy87yxVbXi4s-n-9ZU3uaFCt5Gs")
 OWNER = int(environ.get("OWNER", "7296271316"))
+LOG_CHANNEL = "-1002477114210"
 CREDIT = "यश ठाकुर"
 AUTH_USER = os.environ.get('AUTH_USERS', '7296271316').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
